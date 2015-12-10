7ec172a20c8fdfb0d0d9a2524eabafce382acc10
Andra-Ristei <ristei.andra@gmail.com>
2015-12-10 21:19:05 +0200

    added commands for sensors

--- src/jenny5_command_module.cpp
+++ src/jenny5_command_module.cpp
@@ -39,11 +39,11 @@ bool t_jenny5_command_module::move_motor(int motor_index, int num_steps)
 	return true;
 }
 //--------------------------------------------------------------
-bool t_jenny5_command_module::set_motor_speed(int motor_index, int motor_speed)
+bool t_jenny5_command_module::set_motor_speed(int motor_index, int motor_speed, int motor_acceleration)
 {
 	if (serial_connection.IsOpened()) {
 		char s[20];
-		sprintf(s, "S%d %d#", motor_index, motor_speed);
+		sprintf(s, "S%d %d %d#", motor_index, motor_speed, motor_acceleration);
 		serial_connection.SendData((const char*)s, strlen(s) + 1);
 	}
 	else
@@ -57,6 +57,56 @@ bool t_jenny5_command_module::set_motor_acceleration(int motor_index, int motor_
 	if (serial_connection.IsOpened()) {
 		char s[20];
 		sprintf(s, "A%d %d#", motor_index, motor_acceleration);
+		serial_connection.SendData((const char*)s, strlen(s) + 1);
+	}
+	return true;
+}
+//--------------------------------------------------------------
+bool t_jenny5_command_module::get_ultrasonic_distance(int sensor_index)
+{
+	if (serial_connection.IsOpened()) {
+		char s[20];
+		sprintf(s, "U%d#", sensor_index);
+		serial_connection.SendData((const char*)s, strlen(s) + 1);
+	}
+	return true;
+}
+//--------------------------------------------------------------
+bool t_jenny5_command_module::get_button_status(int button_index)
+{
+	if (serial_connection.IsOpened()) {
+		char s[20];
+		sprintf(s, "B%d#", button_index);
+		serial_connection.SendData((const char*)s, strlen(s) + 1);
+	}
+	return true;
+}
+//--------------------------------------------------------------
+bool t_jenny5_command_module::get_potentiometer_position(int sensor_index)
+{
+	if (serial_connection.IsOpened()) {
+		char s[20];
+		sprintf(s, "P%d#", sensor_index);
+		serial_connection.SendData((const char*)s, strlen(s) + 1);
+	}
+	return true;
+}
+//--------------------------------------------------------------
+bool t_jenny5_command_module::get_infrared_status(int sensor_index)
+{
+	if (serial_connection.IsOpened()) {
+		char s[20];
+		sprintf(s, "I%d#", sensor_index);
+		serial_connection.SendData((const char*)s, strlen(s) + 1);
+	}
+	return true;
+}
+//--------------------------------------------------------------
+bool t_jenny5_command_module::get_motors_sensors_statistics()
+{
+	if (serial_connection.IsOpened()) {
+		char s[20];
+		sprintf(s, "G#");
 		serial_connection.SendData((const char*)s, strlen(s) + 1);
 	}
 	return true;
--- src/jenny5_command_module.h
+++ src/jenny5_command_module.h
@@ -11,8 +11,13 @@ public:
 	bool connect(int port, int baud_rate);
 	void close_connection(void);
 	bool move_motor(int motor_index, int num_steps);
-	bool set_motor_speed(int motor_index, int motor_speed);
+	bool set_motor_speed(int motor_index, int motor_speed, int motor_acceleration);
 	bool set_motor_acceleration(int motor_index, int motor_acceleration);
+	bool get_ultrasonic_distance(int sensor_index);
+	bool get_button_status(int button_index);
+	bool get_potentiometer_position(int sensor_index);
+	bool get_infrared_status(int sensor_index);
+	bool get_motors_sensors_statistics();
 	bool disable_motor(int motor_index);
 	bool lock_motor(int motor_index);
 	bool is_connected(void);
