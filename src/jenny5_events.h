08e4900803a2bf18a7f1ef657132f8bd4ef2a4b3
mihaioltean <moltean@cs.ubbcluj.ro>
2015-12-27 10:16:34 +0200

    a structure for events and a list of types for events

---
+++ src/jenny5_events.h
@@ -0,0 +1,28 @@
+
+#include <stdio.h>
+
+#define IS_ALIVE_EVENT 0
+#define MOTOR_DONE_EVENT 1
+#define ULTRASOUND_EVENT 2
+#define POTENTIOMETER_EVENT 3
+#define BUTTON_EVENT 4
+#define MOTOR_LOCKED_EVENT 5
+#define MOTOR_DISABLED_EVENT 6
+
+//-----------------------------------------------------------------------
+class jenny5_event{
+public:
+	char type;
+	intptr_t param1, param2;
+	int time;
+
+public:
+	jenny5_event(char _type, intptr_t _param1, intptr_t _param2, int _time)
+	{
+		type = _type;
+		param1 = _param1;
+		param2 = _param2;
+		time = _time;
+	}
+};
+//-----------------------------------------------------------------------
