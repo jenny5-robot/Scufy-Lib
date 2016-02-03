ab1d929baccb633592451885d14223fd376ac340
Mihai Oltean <mihai.oltean@gmail.com>
2016-02-03 20:20:39 +0200

    wave test done.

---
+++ src/head_controller.cpp
@@ -0,0 +1,79 @@
+#include "../include/head_controller.h"
+
+//----------------------------------------------------------------
+t_head_controller::t_head_controller(void) {
+	if (connect()) {
+
+		printf("Connected...\n");
+	}
+	if (setup()) {
+		printf("Setup complete...\n");
+	}
+}
+//----------------------------------------------------------------
+bool t_head_controller::connect() {
+	//connect to serial ports
+	if (!head_motors_controller.connect(3, 115200))
+	{
+		std::cout << ("Error attaching to Jenny 5' head motors!\n");
+	}
+
+	jenny5_event connect_to_head_motors_event(IS_ALIVE_EVENT);
+
+	return head_motors_controller.wait_for_command_completion(connect_to_head_motors_event);
+}
+//----------------------------------------------------------------
+bool t_head_controller::setup() {
+	//create the motor controllers
+	int head_motors_dir_pins[2] = { 2, 5 };
+	int head_motors_step_pins[2] = { 3, 6 };
+	int head_motors_enable_pins[2] = { 4, 7 };
+	head_motors_controller.send_create_motors(2, head_motors_dir_pins, head_motors_step_pins, head_motors_enable_pins);
+
+	jenny5_event create_head_motors_event(MOTORS_CONTROLLER_CREATED_EVENT);
+	return head_motors_controller.wait_for_command_completion(create_head_motors_event);
+}
+//----------------------------------------------------------------
+void t_head_controller::move_left(unsigned int num_steps) {
+	head_motors_controller.send_move_motor(MOVE_X, DIRECTION_LEFT * num_steps);
+	head_motors_controller.set_motor_state(MOVE_X, COMMAND_SENT);
+	printf("move head left: M%d %d# - sent\n", MOVE_X, DIRECTION_LEFT * num_steps);
+
+	jenny5_event motor_done_event(MOTOR_DONE_EVENT);
+	head_motors_controller.wait_for_command_completion(motor_done_event);
+}
+//----------------------------------------------------------------
+void t_head_controller::move_right(unsigned int num_steps) {
+	head_motors_controller.send_move_motor(MOVE_X, DIRECTION_RIGTH * num_steps);
+	head_motors_controller.set_motor_state(MOVE_X, COMMAND_SENT);
+	printf("move head right: M%d %d# - sent\n", MOVE_X, DIRECTION_RIGTH * num_steps);
+
+	jenny5_event motor_done_event(MOTOR_DONE_EVENT);
+	head_motors_controller.wait_for_command_completion(motor_done_event);
+
+}
+//----------------------------------------------------------------
+void t_head_controller::move_up(unsigned int num_steps) {
+	head_motors_controller.send_move_motor(MOVE_Y, DIRECTION_UP * num_steps);
+	head_motors_controller.set_motor_state(MOVE_Y, COMMAND_SENT);
+	printf("move head up: M%d %d# - sent\n", MOVE_Y, DIRECTION_UP * num_steps);
+
+	jenny5_event motor_done_event(MOTOR_DONE_EVENT);
+	head_motors_controller.wait_for_command_completion(motor_done_event);
+
+	std::cout << "test\n";
+}
+//----------------------------------------------------------------
+void t_head_controller::move_down(unsigned int num_steps) {
+	head_motors_controller.send_move_motor(MOVE_Y, DIRECTION_DOWN * num_steps);
+	head_motors_controller.set_motor_state(MOVE_Y, COMMAND_SENT);
+	printf("move head down: M%d %d# - sent\n", MOVE_Y, DIRECTION_DOWN * num_steps);
+
+	jenny5_event motor_done_event(MOTOR_DONE_EVENT);
+	head_motors_controller.wait_for_command_completion(motor_done_event);
+}
+//----------------------------------------------------------------
+t_head_controller::~t_head_controller(void) {
+	head_motors_controller.close_connection();
+}
+//----------------------------------------------------------------
--- tests/right_arm_movements.cpp
+++ tests/right_arm_movements.cpp
@@ -6,10 +6,12 @@ int main(void) {
 	t_arm_controller arm_controller;
 
 	arm_controller.lift_shoulder(-400, NO_WAIT);
-	arm_controller.lift_elbow(200, NO_WAIT);
+	arm_controller.lift_elbow(400, NO_WAIT);
 	arm_controller.rotate_upper_arm(150, ELBOW_LIFT);
 	arm_controller.rotate_upper_arm(-300, UPPER_ARM_ROTATE);
 	arm_controller.rotate_upper_arm(300, UPPER_ARM_ROTATE);
+	arm_controller.lift_shoulder(400, UPPER_ARM_ROTATE);
+	arm_controller.lift_elbow(-400, NO_WAIT);
 	arm_controller.wait_for_action(UPPER_ARM_ROTATE);
 	return 0;
 }
