d31e67720ee0df1e1f645cd12fdb976ec1139046
cuvidk <cuvidk@yahoo.ro>
2016-02-19 16:19:52 +0200

    fixes head_controller to correspond with the modified API

--- src/head_controller.cpp
+++ src/head_controller.cpp
@@ -28,46 +28,46 @@ bool t_head_controller::setup() {
 	int head_motors_dir_pins[2] = { 2, 5 };
 	int head_motors_step_pins[2] = { 3, 6 };
 	int head_motors_enable_pins[2] = { 4, 7 };
-	head_motors_controller.send_create_motors(2, head_motors_dir_pins, head_motors_step_pins, head_motors_enable_pins);
+	head_motors_controller.send_create_stepper_motors(2, head_motors_dir_pins, head_motors_step_pins, head_motors_enable_pins);
 
-	jenny5_event create_head_motors_event(MOTORS_CONTROLLER_CREATED_EVENT);
+	jenny5_event create_head_motors_event(STEPPER_MOTORS_CONTROLLER_CREATED_EVENT);
 	return head_motors_controller.wait_for_command_completion(create_head_motors_event);
 }
 //----------------------------------------------------------------
 void t_head_controller::move_left(unsigned int num_steps) {
-	head_motors_controller.send_move_motor(MOVE_X, DIRECTION_LEFT * num_steps);
-	head_motors_controller.set_motor_state(MOVE_X, COMMAND_SENT);
+	head_motors_controller.send_move_stepper_motor(MOVE_X, DIRECTION_LEFT * num_steps);
+	head_motors_controller.set_stepper_motor_state(MOVE_X, COMMAND_SENT);
 	printf("move head left: M%d %d# - sent\n", MOVE_X, DIRECTION_LEFT * num_steps);
 
-	jenny5_event motor_done_event(MOTOR_DONE_EVENT);
+	jenny5_event motor_done_event(STEPPER_MOTOR_MOVE_DONE_EVENT);
 	head_motors_controller.wait_for_command_completion(motor_done_event);
 }
 //----------------------------------------------------------------
 void t_head_controller::move_right(unsigned int num_steps) {
-	head_motors_controller.send_move_motor(MOVE_X, DIRECTION_RIGTH * num_steps);
-	head_motors_controller.set_motor_state(MOVE_X, COMMAND_SENT);
+	head_motors_controller.send_move_stepper_motor(MOVE_X, DIRECTION_RIGTH * num_steps);
+	head_motors_controller.set_stepper_motor_state(MOVE_X, COMMAND_SENT);
 	printf("move head right: M%d %d# - sent\n", MOVE_X, DIRECTION_RIGTH * num_steps);
 
-	jenny5_event motor_done_event(MOTOR_DONE_EVENT);
+	jenny5_event motor_done_event(STEPPER_MOTOR_MOVE_DONE_EVENT);
 	head_motors_controller.wait_for_command_completion(motor_done_event);
 
 }
 //----------------------------------------------------------------
 void t_head_controller::move_up(unsigned int num_steps) {
-	head_motors_controller.send_move_motor(MOVE_Y, DIRECTION_UP * num_steps);
-	head_motors_controller.set_motor_state(MOVE_Y, COMMAND_SENT);
+	head_motors_controller.send_move_stepper_motor(MOVE_Y, DIRECTION_UP * num_steps);
+	head_motors_controller.set_stepper_motor_state(MOVE_Y, COMMAND_SENT);
 	printf("move head up: M%d %d# - sent\n", MOVE_Y, DIRECTION_UP * num_steps);
 
-	jenny5_event motor_done_event(MOTOR_DONE_EVENT);
+	jenny5_event motor_done_event(STEPPER_MOTOR_MOVE_DONE_EVENT);
 	head_motors_controller.wait_for_command_completion(motor_done_event);
 }
 //----------------------------------------------------------------
 void t_head_controller::move_down(unsigned int num_steps) {
-	head_motors_controller.send_move_motor(MOVE_Y, DIRECTION_DOWN * num_steps);
-	head_motors_controller.set_motor_state(MOVE_Y, COMMAND_SENT);
+	head_motors_controller.send_move_stepper_motor(MOVE_Y, DIRECTION_DOWN * num_steps);
+	head_motors_controller.set_stepper_motor_state(MOVE_Y, COMMAND_SENT);
 	printf("move head down: M%d %d# - sent\n", MOVE_Y, DIRECTION_DOWN * num_steps);
 
-	jenny5_event motor_done_event(MOTOR_DONE_EVENT);
+	jenny5_event motor_done_event(STEPPER_MOTOR_MOVE_DONE_EVENT);
 	head_motors_controller.wait_for_command_completion(motor_done_event);
 }
 //----------------------------------------------------------------
