3671efb89c2df5fcf895b4e5a8d5bdc70eb486b1
cuvidk <cuvidk@yahoo.ro>
2016-02-19 14:08:14 +0200

    updates arm movement class

--- src/arm_controller.cpp
+++ src/arm_controller.cpp
@@ -34,14 +34,14 @@ bool t_arm_controller::setup() {
 	int upper_arm_motors_dir_pins[4] = { 2, 5, 8, 11 };
 	int upper_arm_motors_step_pins[4] = { 3, 6, 9, 12 };
 	int upper_arm_motors_enable_pins[4] = { 4, 7, 10, 13 };
-	upper_motors_controller.send_create_motors(4, upper_arm_motors_dir_pins, upper_arm_motors_step_pins, upper_arm_motors_enable_pins);
+	upper_motors_controller.send_create_stepper_motors(4, upper_arm_motors_dir_pins, upper_arm_motors_step_pins, upper_arm_motors_enable_pins);
 
 	int lower_arm_motors_dir_pins[1] = { 5 };
 	int lower_arm_motors_step_pins[1] = { 6 };
 	int lower_arm_motors_enable_pins[1] = { 7 };
-	lower_motors_controller.send_create_motors(1, lower_arm_motors_dir_pins, lower_arm_motors_step_pins, lower_arm_motors_enable_pins);
+	lower_motors_controller.send_create_stepper_motors(1, lower_arm_motors_dir_pins, lower_arm_motors_step_pins, lower_arm_motors_enable_pins);
 
-	jenny5_event create_upper_motors_event(MOTORS_CONTROLLER_CREATED_EVENT);
+	jenny5_event create_upper_motors_event(STEPPER_MOTORS_CONTROLLER_CREATED_EVENT);
 	return upper_motors_controller.wait_for_command_completion(create_upper_motors_event);
 }
 //----------------------------------------------------------------
@@ -50,7 +50,7 @@ void t_arm_controller::wait_for_action(int wait_for)
 {
 	if (wait_for != NO_WAIT)
 	{
-		jenny5_event motor_done_event(MOTOR_DONE_EVENT, wait_for);
+		jenny5_event motor_done_event(STEPPER_MOTOR_MOVE_DONE_EVENT, wait_for);
 
 		if (wait_for == LOWER_ARM_ROTATE)
 		{
@@ -67,8 +67,8 @@ void t_arm_controller::wait_for_action(int wait_for)
 void t_arm_controller::spin_shoulder(int num_steps, int wait_for)
 {
 	wait_for_action(wait_for);
-	upper_motors_controller.send_move_motor(SHOULDER_SPIN, num_steps);
-	upper_motors_controller.set_motor_state(SHOULDER_SPIN, COMMAND_SENT);
+	upper_motors_controller.send_move_dc_motor(SHOULDER_SPIN, num_steps);
+	upper_motors_controller.set_dc_motor_state(SHOULDER_SPIN, COMMAND_SENT);
 	printf("shoulder spin: M%d %d# - sent\n", SHOULDER_SPIN, num_steps);
 }
 //----------------------------------------------------------------
@@ -76,8 +76,8 @@ void t_arm_controller::spin_shoulder(int num_steps, int wait_for)
 void t_arm_controller::lift_shoulder(int num_steps, int wait_for)
 {
 	wait_for_action(wait_for);
-	upper_motors_controller.send_move_motor(SHOULDER_LIFT, num_steps);
-	upper_motors_controller.set_motor_state(SHOULDER_LIFT, COMMAND_SENT);
+	upper_motors_controller.send_move_dc_motor(SHOULDER_LIFT, num_steps);
+	upper_motors_controller.set_dc_motor_state(SHOULDER_LIFT, COMMAND_SENT);
 	printf("shoulder lift: M%d %d# - sent\n", SHOULDER_LIFT, num_steps);
 }
 //----------------------------------------------------------------
@@ -85,8 +85,8 @@ void t_arm_controller::lift_shoulder(int num_steps, int wait_for)
 void t_arm_controller::rotate_upper_arm(int num_steps, int wait_for)
 {
 	wait_for_action(wait_for);
-	upper_motors_controller.send_move_motor(UPPER_ARM_ROTATE, num_steps);
-	upper_motors_controller.set_motor_state(UPPER_ARM_ROTATE, COMMAND_SENT);
+	upper_motors_controller.send_move_dc_motor(UPPER_ARM_ROTATE, num_steps);
+	upper_motors_controller.set_dc_motor_state(UPPER_ARM_ROTATE, COMMAND_SENT);
 	printf("upper arm rotate: M%d %d# - sent\n", UPPER_ARM_ROTATE, num_steps);
 }
 //----------------------------------------------------------------
@@ -94,8 +94,8 @@ void t_arm_controller::rotate_upper_arm(int num_steps, int wait_for)
 void t_arm_controller::rotate_lower_arm(int num_steps, int wait_for)
 {
 	wait_for_action(wait_for);
-	lower_motors_controller.send_move_motor(LOWER_ARM_ROTATE, num_steps);
-	lower_motors_controller.set_motor_state(LOWER_ARM_ROTATE, COMMAND_SENT);
+	lower_motors_controller.send_move_dc_motor(LOWER_ARM_ROTATE, num_steps);
+	lower_motors_controller.set_dc_motor_state(LOWER_ARM_ROTATE, COMMAND_SENT);
 	printf("lower arm rotate: M%d %d# - sent\n", LOWER_ARM_ROTATE, num_steps);
 }
 //----------------------------------------------------------------
@@ -103,8 +103,8 @@ void t_arm_controller::rotate_lower_arm(int num_steps, int wait_for)
 void t_arm_controller::lift_elbow(int num_steps, int wait_for)
 {
 	wait_for_action(wait_for);
-	upper_motors_controller.send_move_motor(ELBOW_LIFT, num_steps);
-	upper_motors_controller.set_motor_state(ELBOW_LIFT, COMMAND_SENT);
+	upper_motors_controller.send_move_dc_motor(ELBOW_LIFT, num_steps);
+	upper_motors_controller.set_dc_motor_state(ELBOW_LIFT, COMMAND_SENT);
 	printf("elbow lift: M%d %d# - sent\n", ELBOW_LIFT, num_steps);
 }
 //----------------------------------------------------------------
